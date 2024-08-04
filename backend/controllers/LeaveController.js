403ab56aa2e71295e81b4b1e8345fc86d7ea9d86
Teddy Lelong <teddy.lelong@gmail.com>
2024-08-04 17:47:15 +0200

    feat(backend): leaves are populated with Employee

--- backend/controllers/LeaveController.js
+++ backend/controllers/LeaveController.js
@@ -5,6 +5,16 @@ class LeaveController extends CrudController {
   constructor() {
     super(Leave);
   }
+
+  // Override the getAll method to use populate
+  getAll = async (req, res) => {
+    try {
+      const leaves = await Leave.find().populate("employee");
+      res.status(200).json(leaves);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
 }
 
 module.exports = new LeaveController();
