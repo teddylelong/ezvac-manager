73ab4de15a300c9e74c98a9f5b0a64276c11df8c
Teddy Lelong <teddy.lelong@gmail.com>
2024-09-01 15:14:31 +0200

    feat(backend): remove employee.leaves on employee deletion

--- backend/controllers/EmployeeController.js
+++ backend/controllers/EmployeeController.js
@@ -1,10 +1,30 @@
 const CrudController = require("./CrudController");
 const Employee = require("../models/Employee");
+const Leave = require("../models/Leave");
 
 class EmployeeController extends CrudController {
   constructor() {
     super(Employee);
   }
+
+  delete = async (req, res) => {
+    try {
+      const employee = await this.model.findById(req.params.id);
+      if (!employee) {
+        return res
+          .status(404)
+          .json({ message: `${this.model.modelName} Not Found` });
+      }
+      await Leave.deleteMany({ employee: employee._id });
+      await this.model.findByIdAndDelete(req.params.id);
+
+      res
+        .status(200)
+        .json({ message: `${this.model.modelName} deleted successfully` });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  };
 }
 
 module.exports = new EmployeeController();
