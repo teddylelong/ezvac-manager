63fa5448aa4f6706bbb5c3e185795372df29c32f
Teddy Lelong <teddy.lelong@gmail.com>
2024-09-26 22:44:33 +0200

    feat(#4): add SettingItem component

--- frontend/src/components/settings/SettingsOverview.js
+++ frontend/src/components/settings/SettingsOverview.js
@@ -6,6 +6,7 @@ import Modal from "../common/Modal.js";
 import Button from "../common/Button.js";
 import SettingsForm from "./SettingsForm.js";
 import Spinner from "../common/Spinner.js";
+import SettingItem from "./SettingItem.js";
 
 const SettingsOverview = () => {
   const [settings, setSettings] = useState([]);
@@ -57,60 +58,7 @@ const SettingsOverview = () => {
           <Spinner />
         ) : (
           settings.map((setting) => (
-            <div
-              key={setting._id}
-              className="bg-white dark:bg-gray-800 rounded-lg shadow-lg border border-gray-300 dark:border-gray-700 overflow-hidden"
-            >
-              <div className="bg-gray-300 dark:bg-gray-700 p-4">
-                <h2 className="text-2xl text-gray-800 dark:text-white font-semibold text-center">
-                  {setting.year}
-                </h2>
-              </div>
-
-              <div className="p-4">
-                <div className="mb-4">
-                  <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200 mb-2">
-                    Excluded Dates
-                  </h3>
-                  <ul className="list-disc list-inside text-gray-800 dark:text-white space-y-1">
-                    {setting.excludedDates.length > 0 ? (
-                      setting.excludedDates.map((excludedDate, index) => (
-                        <li key={index}>
-                          {new Date(excludedDate).toLocaleDateString()}
-                        </li>
-                      ))
-                    ) : (
-                      <p className="italic text-gray-500 dark:text-gray-400">
-                        No excluded dates
-                      </p>
-                    )}
-                  </ul>
-                </div>
-
-                <div>
-                  <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200 mb-2">
-                    Excluded Date Intervals
-                  </h3>
-                  <ul className="list-disc list-inside text-gray-800 dark:text-white space-y-1">
-                    {setting.excludedDateIntervals.length > 0 ? (
-                      setting.excludedDateIntervals.map((interval, index) => (
-                        <li key={index}>
-                          {`${new Date(
-                            interval.startDate
-                          ).toLocaleDateString()} → ${new Date(
-                            interval.endDate
-                          ).toLocaleDateString()}`}
-                        </li>
-                      ))
-                    ) : (
-                      <p className="italic text-gray-500 dark:text-gray-400">
-                        No excluded intervals
-                      </p>
-                    )}
-                  </ul>
-                </div>
-              </div>
-            </div>
+            <SettingItem key={setting._id} setting={setting} />
           ))
         )}
       </section>
