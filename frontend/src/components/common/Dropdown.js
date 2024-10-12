3bc84df62bcffa43ff4230dbfb97aae9db23b980
Teddy Lelong <teddy.lelong@gmail.com>
2024-10-12 15:02:22 +0200

    feat(#3): Dropdown now supports custom icon and HTML content

--- frontend/src/components/common/Dropdown.js
+++ frontend/src/components/common/Dropdown.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, children, icon, title = "" }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [confirmingOption, setConfirmingOption] = useState(null);
   const dropdownRef = useRef(null);
@@ -32,7 +32,6 @@ const Dropdown = ({ options }) => {
 
   const handleOptionClick = (option) => {
     if (option.danger && !confirmingOption) {
-      // option danger needs confirmation
       setConfirmingOption(option);
     } else if (confirmingOption && confirmingOption === option) {
       option.onClick();
@@ -46,30 +45,39 @@ const Dropdown = ({ options }) => {
   return (
     <div className="relative inline-block text-left" ref={dropdownRef}>
       <button
+        title={title}
         onClick={toggleDropdown}
         className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 hover:text-sky-400 dark:text-gray-200 dark:hover:text-sky-400"
       >
-        <FontAwesomeIcon icon={faEllipsis} />
+        {icon ? (
+          <FontAwesomeIcon icon={icon} />
+        ) : (
+          <FontAwesomeIcon icon={faEllipsis} />
+        )}
       </button>
 
       {isOpen && (
         <div className="absolute right-0 z-10 mt-2 w-48 bg-white dark:bg-gray-600 rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
           <div className="py-1">
-            {options.map((option, index) => (
-              <button
-                key={index}
-                onClick={() => handleOptionClick(option)}
-                className={`block w-full text-left px-4 py-2 text-sm ${
-                  option.danger
-                    ? "text-red-600 dark:text-red-400 hover:bg-red-100 dark:hover:bg-red-800 dark:hover:text-gray-200"
-                    : "text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
-                }`}
-              >
-                {confirmingOption === option
-                  ? `Are you sure? Click again to confirm.`
-                  : option.label}
-              </button>
-            ))}
+            {children ? (
+              <div>{children}</div>
+            ) : (
+              options.map((option, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleOptionClick(option)}
+                  className={`block w-full text-left px-4 py-2 text-sm ${
+                    option.danger
+                      ? "text-red-600 dark:text-red-400 hover:bg-red-100 dark:hover:bg-red-800 dark:hover:text-gray-200"
+                      : "text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-500"
+                  }`}
+                >
+                  {confirmingOption === option
+                    ? `Are you sure? Click again to confirm.`
+                    : option.label}
+                </button>
+              ))
+            )}
           </div>
         </div>
       )}
@@ -84,7 +92,9 @@ Dropdown.propTypes = {
       onClick: PropTypes.func.isRequired,
       danger: PropTypes.bool,
     })
-  ).isRequired,
+  ),
+  children: PropTypes.node,
+  icon: PropTypes.object,
 };
 
 export default Dropdown;
